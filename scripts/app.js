c7cba0411e2a97478dacd84728eb0e37ad669c08
Chris Aquino <chris@radishmouse.com>
2021-01-19 15:29:37 -0500

    adds async await version

--- scripts/app.js
+++ scripts/app.js
@@ -1,41 +1,16 @@
-console.log('hello there lovely people');
-
-function processResponse(response) {
-    return response.json();
+async function main() {
+    // Show the joke on the page:
+    // 1. make the Request using fetch()
+    const jokePromise = fetch('https://icanhazdadjoke.com', {
+        headers: {
+            Accept: 'application/json'
+        }
+    });
+    const response = await jokePromise;
+    // 2. grab the .joke
+    const jokeData = await response.json();
+    console.log(jokeData);
+    console.log(jokeData.joke);
+    // 3. render it to the page
 }
-
-// Show the joke on the page:
-// 1. make the Request using fetch()
-fetch('https://icanhazdadjoke.com', {
-    headers: {
-        Accept: 'application/json'
-    }
-})
-.then(response => {
-    console.log(response);
-    // let parsedResp = JSON.parse(response);
-    // console.log(parsedResp);
-    const pendingData = response.json();
-
-    console.log(pendingData);
-    return pendingData; // returns to the next .then()
-}) // convert to JSON
-.then(finishedData => {
-    // data is a plain JavaScript Object
-    console.log(finishedData);
-    return finishedData.joke;
-})
-.then(theJoke => {
-    console.log(theJoke);
-})
-.catch(err => console.log(err))
-// 2. grab the .joke
-// 3. render it to the page
-
-// Alternative to use .catch() is to pass a second callback to .then()
-// fetch('https://icanhazdadjoke.com', {
-//     headers: {
-//         Accept: 'application/json'
-//     }
-// })
-// .then(response => response.json(), err => console.log(err))
+main();
