1d42ea80f67a8a936023adb18deb1ccd6f104905
Chris Aquino <chris@radishmouse.com>
2021-01-19 16:22:21 -0500

    adds renderJoke

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,13 @@
-async function main() {
+/*
+How do you get an "as-you-type" feature in your front-end app?
+example: as you type a username into a signup form, it tells you if that username is taken
+1. addEventListener
+2. fetch()
+3. render
+*/
+
+
+async function getJoke() {
     // Show the joke on the page:
     // 1. make the Request using fetch()
     const jokePromise = fetch('https://icanhazdadjoke.com', {
@@ -9,8 +18,22 @@ async function main() {
     const response = await jokePromise;
     // 2. grab the .joke
     const jokeData = await response.json();
-    console.log(jokeData);
-    console.log(jokeData.joke);
+    // console.log(jokeData);
+    // console.log(jokeData.joke);
     // 3. render it to the page
+    return jokeData.joke;
 }
-main();
+
+async function renderJoke() {
+    // async b/c we will await getJoke
+    const joke = await getJoke();
+
+    // create the DOM element    
+    const p = document.createElement('p');
+
+    // put the joke text into it
+    p.textContent = joke;
+
+    // attach it to an existing DOM element
+    document.getElementById('root').appendChild(p);
+}
